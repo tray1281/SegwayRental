cbd8a5869d9d785d763e81b4f6be25066267d1e6
Toure Edwards <tray1281@hotmail.com>
2019-04-15 22:26:12 -0400

    testing

--- app/src/main/java/net/androidbootcamp/segwayrental/Tour.java
+++ app/src/main/java/net/androidbootcamp/segwayrental/Tour.java
@@ -9,5 +9,6 @@ public class Tour extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour);
+        //this is me testing
     }
 }
